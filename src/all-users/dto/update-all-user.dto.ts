14017c316e68823ae8acb4de97974e47df8e3226
Hossain, Musharuf <21-45112-2@student.aiub.edu>
2024-06-09 00:41:58 +0600

    new

--- src/all-users/dto/update-all-user.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAllUserDto } from './create-all-user.dto';
-
-export class UpdateAllUserDto extends PartialType(CreateAllUserDto) {}
--- src/newcourse/dto/update-newcourse.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateNewcourseDto } from './create-newcourse.dto';
-
-export class UpdateNewcourseDto extends PartialType(CreateNewcourseDto) {}
